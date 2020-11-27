edfc564112a21823efb56bbde6014763977f66c8
Inumbratus <36467574+Inumbratus@users.noreply.github.com>
2020-11-27 23:10:57 +1100

    Working on target hardware
    - The Project now works on target hardware, basic disappearing message has been added

---
+++ Assets/UIHandlerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHandlerScript : MonoBehaviour
+{
+    float CurrentTime = 0.0f;
+    public float MaxTime = 15.0f;
+    public Object UIMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentTime = MaxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentTime -= 1 * Time.deltaTime;
+        if(CurrentTime <= 0.0f)
+        {
+            Destroy(UIMessage);
+        }
+    }
+}
